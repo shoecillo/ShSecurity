8736a748c8847c08bbfc3bc074aee2f27eb1dc30
shoecillo <shoecillo@gmail.com>
2017-09-14 17:49:41 +0200

    Add CORS Filter

--- ShSecureGateway/src/main/java/com/sh/secure/app/SecureGateway.java
+++ ShSecureGateway/src/main/java/com/sh/secure/app/SecureGateway.java
@@ -10,6 +10,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.sh.auth.crypto.RSAKeysUtil;
 import com.sh.auth.crypto.RSARepository;
@@ -58,6 +61,24 @@ public class SecureGateway {
 	{
 		SpringApplication.run(SecureGateway.class, args);
 		LOGGER.debug("APPLICATION STARTED");
+	}
+	
+	@Bean
+	public CorsFilter corsFilter() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    final CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.addAllowedOrigin("*");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("OPTIONS");
+	    config.addAllowedMethod("HEAD");
+	    config.addAllowedMethod("GET");
+	    config.addAllowedMethod("PUT");
+	    config.addAllowedMethod("POST");
+	    config.addAllowedMethod("DELETE");
+	    config.addAllowedMethod("PATCH");
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
 	}
 	
 	/**
